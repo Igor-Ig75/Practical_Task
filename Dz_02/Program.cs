e74c5d9d92b6c6c1ee4476638700f4d88abddd03
igor <igoshinig@yandex.ru>
2022-07-04 14:42:29 +0500

    Второе задание

--- Dz_02/Program.cs
+++ Dz_02/Program.cs
@@ -1,7 +1,6 @@
 ﻿/*
 Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
 и на выходе показывает вторую цифру этого числа.
-
 456 -> 5
 782 -> 8
 918 -> 1
@@ -10,7 +9,7 @@
 int CutNumber()  
 {
     int num = new Random().Next(100,1000);   
-    Console.WriteLine("Current random number is " + num);  
+    Console.WriteLine("Случайное число " + num);  
 
     int ten = num % 100;  
     int ed = ten / 10;
@@ -20,19 +19,52 @@ int CutNumber()
 }
 
 int number = CutNumber();   
-Console.WriteLine("Result number is " + number);  
-
+Console.WriteLine("Вторая цифра случайного числа " + number);  
 
 /*
 Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 что третьей цифры нет.
-
 645 -> 5
-
 78 -> третьей цифры нет
-
 32679 -> 6
 */
+
+int GenNum() 
+{
+    return new Random().Next();
+}
+
+int[] GetArray(int num) 
+{
+
+    string n = num.ToString();
+    int[] arr = new int[n.Length];
+    int i = 0;
+
+    while(i < n.Length) 
+    {
+        arr[i] = Convert.ToInt32(n[i].ToString());
+        i++;
+    }
+
+       return arr;
+}
+
+int GetThirdDigit(int[] arr) 
+{
+    if(arr.Length < 3) 
+    {
+        Console.WriteLine("Цифры нет");
+        return -1;
+    }
+
+    return arr[2];
+}
+
+int n = GenNum();
+Console.WriteLine(n);
+Console.WriteLine(GetThirdDigit(GetArray(n)));
+
 /*
 Задача 15: Напишите программу, которая принимает на вход цифру, 
 обозначающую день недели, и проверяет, является ли этот день выходным.
@@ -60,6 +92,6 @@ void DayWeek (int number)
 
 }
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-DayWeek(num);
+//Console.Write("Введите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//DayWeek(num);
